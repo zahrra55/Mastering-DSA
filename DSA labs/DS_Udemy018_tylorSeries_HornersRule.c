a6f5f0b46828a99aa8a34001963ae36a340a8ef6
Your Name <your.email@example.com>
2025-03-29 09:30:57 +0300

    Add Taylor Series implementation using Horner's Rule

---
+++ DSA labs/DS_Udemy018_tylorSeries_HornersRule.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+// Taylor Series using Static variables
+
+double e(int x, int n)
+{
+    static double s = 1;
+    if(n == 0)
+        return s;
+    s = 1 + x * s/ n ;
+    //s = 1 + x / n * s;
+    return e(x, n - 1);
+}
+
+double e_2(int x, int n)
+{
+    double s = 1;
+    for(; n > 0; n--)
+    s = 1 + x * s/ n ;
+    return s;
+}
+
+double e_iterative(int x, int n)
+{
+    double s=1, num = 1, den = 1;
+
+ for(int i=1;i<=n;i++)
+ {
+     num*=x;
+     den*=i;
+     s+=num/den;
+    }
+     return s;
+}
+int main()
+{
+    printf("%lf \n",e(2, 10));
+    printf("%lf \n",e_iterative(2, 10));
+    printf("%lf \n",e_2(2, 10));
+
+    return 0;
+}
