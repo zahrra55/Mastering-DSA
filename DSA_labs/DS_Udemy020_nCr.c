dd15475b561164ab8a00ae85aab7465f51dde20f
Your Name <your.email@example.com>
2025-04-03 00:29:29 +0300

    nCr update

--- DSA_labs/DS_Udemy020_nCr.c
+++ DSA_labs/DS_Udemy020_nCr.c
@@ -1,5 +1,5 @@
 // This is nCr program using recursion
-// The nCr Formula is n!/(r!(n-r)!)
+
 #include<stdio.h>
 
 int factorial(int n)
@@ -10,7 +10,8 @@ int factorial(int n)
         return factorial(n - 1) * n;
 }
 
-int iterative_C(int n, int r)
+// The nCr Formula is n!/(r!(n-r)!)
+int iterative_nCr(int n, int r)
 {
     int t1, t2, t3;
     t1 = factorial(n);
@@ -19,14 +20,26 @@ int iterative_C(int n, int r)
     return t1 / (t2 * t3);
 }
 
+int recursive_nCr(int n, int r)
+{
+    if (n == r || r == 0)
+        return 1;
+    else
+        return recursive_nCr(n - 1, r - 1) + recursive_nCr(n - 1, r);
+}
+
 int main()
 {
     int n, r;
-    while (1)
+    char choice;
+    do
     {
         printf("Enter n and r: ");
         scanf("%d %d", &n, &r);
-        printf("nCr of %d and %d is: %d\n\n", n, r, iterative_C(n, r));
-    }
+        printf("iterative nCr of %d and %d is: %d\n\n", n, r, iterative_nCr(n, r));
+        printf("recursive nCr of %d and %d is: %d\n\n", n, r, recursive_nCr(n, r));
+        printf("Do you want to continue? (y/n): ");
+        scanf(" %c", &choice);
+    } while (choice == 'y' || choice == 'Y');
     return 0;
 }
