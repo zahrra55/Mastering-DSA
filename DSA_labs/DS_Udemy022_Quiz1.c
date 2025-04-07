fc14043fcf4ade028e561911f0a84028b939376f
Zahraa Ibrahim <shugo7385@gmail.com>
2025-04-08 00:46:33 +0300

    updating the quize Qs

--- DSA_labs/DS_Udemy022_Quiz1.c
+++ DSA_labs/DS_Udemy022_Quiz1.c
@@ -1,8 +1,7 @@
-// The Recursion final Quize 
+// The Recursion final Quize Answers
 
 #include<stdio.h>
 
-// Q1- what's the return value of Q1_fun(1)?
 int Q1_fun(int n)
 {
     static int i = 0;
@@ -11,10 +10,9 @@ int Q1_fun(int n)
     n += i;
     i++;
     return Q1_fun(n);
-    // The final result will be: 7
+    // The final result of Q1_fun(1) will be: 7
 }
 
-// Q2- what's the return value of Q2_fun(2048, 0)?
 int Q2_fun(int n, int sum)
 {
     int k = 0, j = 0;
@@ -25,13 +23,50 @@ int Q2_fun(int n, int sum)
     sum += k;
     Q2_fun(j, sum);
     printf("%d ", k); 
+    // The final result of Q2_fun(2048, 0) will be: 20480
+}
+
+int Q3_fun(int &x, int c)
+{
+    c -= 1;
+    if (c == 0)
+        return 1;
+    x += 1;
+    return Q3_fun(x, c) * x;
+    // The final result of Q5_fun(5, 5) will be: 9^4 = 6561
+}
+
+int Q4_fun(int n)
+{
+    int x = 1;
+    if(n == 1)
+        return x;
+    for(int k = 1; k < n; ++k)
+        x = x * x + Q4_fun(k) * Q4_fun(n - k);
+    return x;
+    // The final result of Q4_fun(5) will be: 51
+
+}
+
+int Q5_fun(int n)
+{
+    static int d = 1;
+    printf("%d %d", n, d);
+    d++;
+    if (n > 0)
+        Q5_fun(n - 1);
+    printf("%d ", d);
+    // The final result of Q5_fun(3) will be: 312213444
 }
 int main()
 {
-    int a = 2048, sum = 0;
-    Q1_fun(1);
-    Q2_fun(a, sum);
-    printf("%d", sum);
-    // The final result of Q2_fun will be: 
+    int a = 2048, sum = 0, p = 5;
+
+    printf("Q1 Output is: %d", Q1_fun(1));
+    printf("Q2 Output is: %d", Q2_fun(a, sum));
+    printf("Q3 Output is: %d", Q3_fun(p, p));
+    printf("Q4 Output is: %d", Q4_fun(5));
+    printf("Q5 Output is: %d", Q5_fun());
+    
     return 0;
 }
