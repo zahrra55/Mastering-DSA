7462bd580b0850e81a24f0202171b4c80375227a
Zahraa Ibrahim <shugo7385@gmail.com>
2025-04-13 11:24:07 +0300

    upsating Arrays.c

--- DSA_labs/DS_Udemy023_Arrays.c
+++ DSA_labs/DS_Udemy023_Arrays.c
@@ -1,13 +1,15 @@
-#include<stdio.h>
 // Example of accessing elements inside an array
+
+#include<stdio.h>
+
 int main()
 {
-int A[5] = {2,4,6,8};
-for(int i = 0; i < 5 ; i++)
-{
-printf("A[i]: %d\n", A[i] );
-printf("A[2]: %d\n", A[2] );
-printf("2[A]: %d\n", 2[A] );
-printf("*(A + 2 ): %d\n", *(A + 2 ) );
-}
+    int A[5] = {2,4,6,8};
+    for(int i = 0; i < 5 ; i++)
+    {
+        printf("A[i]: %d\n", A[i] );
+        printf("A[2]: %d\n", A[2] );
+        printf("2[A]: %d\n", 2[A] );
+        printf("*(A + 2 ): %d\n", *(A + 2 ) );
+    }
 }
